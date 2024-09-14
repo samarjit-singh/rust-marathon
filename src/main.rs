b132a518045e7965271051e983106d0f72270569
samarjit-singh <Samarjitbravo@gmail.com>
2024-09-14 13:33:44 +0530

    implement a function on top of struct

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,24 @@
-struct User {  // similar to interface in typescript
-    first_name: String,
-    last_name: String,
-    age: i32
+struct Rect {
+    width: i32,
+    height: i32,
 }
 
+impl Rect {
+    fn area(&self) -> i32 {
+        self.width * self.height // implicite return
+    }
+
+    fn perimeter(&self) -> i32 {
+        2 * (self.width + self.height)
+    }
+}
 
 fn main() {
-    let user = User {
-        first_name: String::from("Samarjit"),
-        last_name: String::from("Singh"),
-        age:22
+    let rect1 = Rect {
+        width: 10,
+        height: 10
     };
 
-    println!("name: {} {} age: {}", user.first_name, user.last_name, user.age);
+    println!("area is {}", rect1.area());
+    println!("perimeter is {}", rect1.perimeter());
 }
-
-
-// fn is_even(num: i32) -> bool {  //u32, i64, u64 i is signed number, u is unsigned number (i means + - )
-//     if num % 2 == 0 {
-//         return true
-//     }
-
-//     return false
-// }
-
-
-// fn fib(num: i32) -> i32 {
-//      let mut first = 0; // mutable variable
-//      let mut second = 1;
-
-//      if num == 0 {
-//         return first;
-//      }
-
-//      if num == 1{
-//         return second;
-//      }
-
-//      for _ in 0..num-1 {
-//         let temp = second;
-//         second = second+first;
-//         first = temp;
-//      }
-
-//      return second;
-// }
-
-// fn get_str_len(str: String) -> usize {
-//     str.chars().count()  //implicit return
-// }
