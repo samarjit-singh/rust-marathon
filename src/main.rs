5188fd4d12eeb6faed6bc6f81a4223ac083aae8b
samarjit-singh <Samarjitbravo@gmail.com>
2024-09-14 13:06:13 +0530

    create fib function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 fn main() {
-    let ans = is_even(11);
+    let ans = fib(4);
     println!("{} hello ji!! ji aaya nu",ans);
 }
 
 
-fn is_even(num: i32) -> bool {  //u32, i64, u64 i is signed number, u is unsigned number (i means + - )
-    if num % 2 == 0 {
-        return true
-    }
+// fn is_even(num: i32) -> bool {  //u32, i64, u64 i is signed number, u is unsigned number (i means + - )
+//     if num % 2 == 0 {
+//         return true
+//     }
 
-    return false
+//     return false
+// }
+
+
+fn fib(num: i32) -> i32 {
+     let mut first = 0; // mutable variable
+     let mut second = 1;
+
+     if num == 0 {
+        return first;
+     }
+
+     if num == 1{
+        return second;
+     }
+
+     for _ in 0..num-1 {
+        let temp = second;
+        second = second+first;
+        first = temp;
+     }
+
+     return second;
 }
